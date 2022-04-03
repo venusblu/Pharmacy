1b04a3324aa5b6a24b63cde05af0eee3dd6a8eeb
venusblu <venusblu@naver.com>
2022-04-03 21:52:56 +0900

    button add

--- app/src/main/java/com/example/pharmacy/MainActivity.java
+++ app/src/main/java/com/example/pharmacy/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView main = findViewById(R.id.textView2);
+        Button loginbt = findViewById(R.id.btn_login);
+        Button regisbt = findViewById(R.id.btn_register);
+
+        loginbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Main.class);
+            }
+        });
+
+        regisbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), regist.class);
+            }
+        });
         main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
