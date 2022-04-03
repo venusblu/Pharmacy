6c16de3fa0c2f38c59402102178d577696102880
venusblu <venusblu@naver.com>
2022-04-03 19:46:16 +0900

    button add

--- app/src/main/java/com/example/pharmacy/MainActivity.java
+++ app/src/main/java/com/example/pharmacy/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.pharmacy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView main = findViewById(R.id.textView2);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
